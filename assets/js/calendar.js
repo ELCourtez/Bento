73b9fade23262cfc47a25e9cf29aaeb7ab01c9db
ELCourtez <5037236+ELCourtez@users.noreply.github.com>
2024-02-06 15:14:20 +0000

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,9 +3,15 @@ getCalendar();
 
 function getCalendar(latitude, longitude) {
             var myHeaders = new Headers();
-            myHeaders.append("Authorization", "Basic " + `${CONFIG.calendarKey}`);
+	    console.log(${CONFIG.calendarKey});
+            myHeaders.append("Authorization", "Basic amJlbmFyZDoxakJHX3UxNEpvc1Q5QzE=");
             myHeaders.append("Cookie", "cookie_test=test; __Host-nc_sameSiteCookielax=true; __Host-nc_sameSiteCookiestrict=true; oc_sessionPassphrase=RoJZ1ye9HwatXWr9VaihFKLwPjOGrdlqzRCn7ajz06c5hyKT%2Bc6NhH8AOpLz%2BIkdd6M1A0TJGh9GsNrGbgYJlT5ppNRJUh9LOUJcDWiJKdm4zk7iQLIfo8FBEZDkkWmA; ocjug6sjwdyv=n0nf2t9bpke8o9f31nhjs0uc9v");
             myHeaders.append("Content-Type", "application/json");
+
+            var myHeader = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic amJlbmFyZDoxakJHX3UxNEpvc1Q5QzE='
+            };
 
             var requestOptions = {
             method: 'GET',
@@ -14,7 +20,7 @@ function getCalendar(latitude, longitude) {
             };
 			
 
-	let api = `${CONFIG.calendarURI}`;
+	let api = `https://cloud.woodphant.fr/remote.php/dav/calendars/jbenard/28c6e092-8c4b-4698-a8e4-713d4ca6894c/?export&accept=jcal`;
 	fetch(api, requestOptions)
 		.then(function(response) {
 			let data = response.json();
