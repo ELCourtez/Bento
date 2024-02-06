645efa9033b711c24cd6ad75b6cafeaded9be115
ELCourtez <5037236+ELCourtez@users.noreply.github.com>
2024-02-06 12:33:55 +0000

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,3 +1,16 @@
+
+const iconElement = document.querySelector('.weatherIcon');
+const tempElement = document.querySelector('.weatherValue p');
+const descElement = document.querySelector('.weatherDescription p');
+
+const weather = {};
+weather.temperature = {
+	unit: 'celsius',
+};
+
+getCalendar();
+
+function getCalendar(latitude, longitude) {
             var myHeaders = new Headers();
             myHeaders.append("Authorization", "Basic amJlbmFyZDoxakJHX3UxNEpvc1Q5QzE=");
             myHeaders.append("Cookie", "cookie_test=test; __Host-nc_sameSiteCookielax=true; __Host-nc_sameSiteCookiestrict=true; oc_sessionPassphrase=RoJZ1ye9HwatXWr9VaihFKLwPjOGrdlqzRCn7ajz06c5hyKT%2Bc6NhH8AOpLz%2BIkdd6M1A0TJGh9GsNrGbgYJlT5ppNRJUh9LOUJcDWiJKdm4zk7iQLIfo8FBEZDkkWmA; ocjug6sjwdyv=n0nf2t9bpke8o9f31nhjs0uc9v");
@@ -13,13 +26,21 @@
             headers: myHeaders,
             redirect: 'follow'
             };
+			
 
-            var calendar = '';
+	let api = `https://cloud.woodphant.fr/remote.php/dav/calendars/jbenard/28c6e092-8c4b-4698-a8e4-713d4ca6894c/?export&accept=jcal`;
+	fetch(api, requestOptions)
+		.then(function(response) {
+			let data = response.json();
+			return data;
+		})
+		.then(function(data) {
+			console.log(data);
+		});
+}
 
-            var mycalendar = fetch("https://cloud.woodphant.fr/remote.php/dav/calendars/jbenard/28c6e092-8c4b-4698-a8e4-713d4ca6894c/?export&accept=jcal", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-
-            console.log("test");
-            console.log(await mycalendar);
+function displayWeather() {
+	iconElement.innerHTML = `<img src="assets/icons/${CONFIG.weatherIcons}/${weather.iconId}.png"/>`;
+	tempElement.innerHTML = `${weather.temperature.value.toFixed(0)}°<span class="darkfg">${tempUnit}</span>`;
+	descElement.innerHTML = weather.description;
+}
