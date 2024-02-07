2cb95fddae656be2d0abb5f54ad9a6f108495f5b
ELCourtez <5037236+ELCourtez@users.noreply.github.com>
2024-02-07 11:47:35 +0000

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -12,11 +12,6 @@ function getCalendar() {
             //myHeaders.append("Cookie", "cookie_test=test; __Host-nc_sameSiteCookielax=true; __Host-nc_sameSiteCookiestrict=true; oc_sessionPassphrase=RoJZ1ye9HwatXWr9VaihFKLwPjOGrdlqzRCn7ajz06c5hyKT%2Bc6NhH8AOpLz%2BIkdd6M1A0TJGh9GsNrGbgYJlT5ppNRJUh9LOUJcDWiJKdm4zk7iQLIfo8FBEZDkkWmA; ocjug6sjwdyv=n0nf2t9bpke8o9f31nhjs0uc9v");
             myHeaders.append("Content-Type", "application/json");
 
-            var myHeader = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Basic amJlbmFyZDoxakJHX3UxNEpvc1Q5QzE='
-            };
-
             var requestOptions = {
             method: 'GET',
             headers: myHeaders,
