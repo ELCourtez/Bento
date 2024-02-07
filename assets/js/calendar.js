4459f5833e05eb2cd353f7380a9241a90f13e7c6
ELCourtez <5037236+ELCourtez@users.noreply.github.com>
2024-02-07 11:21:27 +0000

    Update calendar.js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -52,10 +52,68 @@ function getCalendar() {
 			calendar.events = my_events;
 		})
 		.then(function(){
-			displayCalendar();
+			if(CONFIG.bentoLayout === 'bentocalendar'){
+				generateFirstListsContainerCalendar();
+			}
 		});
 }
+const generateFirstListsContainerCalendar = () => {
 
-function displayCalendar() {
-	console.log(calendar);
-}
+	for (let i = 0; i < 4; i++) 
+	{
+			let item = `
+		<div class="card list list__1" id="list_1">
+		  <i class="listIcon" icon-name="calendar"></i>
+		  <a
+		  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+		  href="#"
+		  class="listItem"
+		  >${calendar.events[0].summary}</a>
+		  <a
+		  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+		  href="#"
+		  class="listItem"
+		  >${calendar.events[1].summary}</a>
+		  <a
+		  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+		  href="#"
+		  class="listItem"
+		  >${calendar.events[2].summary}</a>
+		  <a
+		  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+		  href="#"
+		  class="listItem"
+		  >${calendar.events[3].summary}</a>
+		</div>
+		  `;
+	} 
+
+	lists = CONFIG.firstlistsContainer;
+		let item = item + `
+	<div class="card list list__${lists[1].id}" id="list_${lists[1].id}">
+	  <i class="listIcon" icon-name="${lists[1].icon}"></i>
+	  <a
+	  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+	  href="${lists[1].links[0].link}"
+	  class="listItem"
+	  >${lists[1].links[0].name}</a>
+	  <a
+	  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+	  href="${lists[1].links[1].link}"
+	  class="listItem"
+	  >${lists[1].links[1].name}</a>
+	  <a
+	  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+	  href="${lists[1].links[2].link}"
+	  class="listItem"
+	  >${lists[1].links[2].name}</a>
+	  <a
+	  target="${CONFIG.openInNewTab ? '_blank' : ''}"
+	  href="${lists[1].links[3].link}"
+	  class="listItem"
+	  >${lists[1].links[3].name}</a>
+	</div>
+      `;
+		const position = 'beforeend';
+		lists_1.insertAdjacentHTML(position, item);
+};
